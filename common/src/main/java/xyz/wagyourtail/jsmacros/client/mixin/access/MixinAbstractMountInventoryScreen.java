6d6e3846f7dba3559f3489915b25fc96734893bf
Jack Manning <eastarctica@gmail.com>
2025-11-23 20:27:48 -0500

    Initial update to 1.21.11-pre2

--- common/src/main/java/xyz/wagyourtail/jsmacros/client/mixin/access/MixinAbstractMountInventoryScreen.java
+++ common/src/main/java/xyz/wagyourtail/jsmacros/client/mixin/access/MixinAbstractMountInventoryScreen.java
@@ -1,22 +1,21 @@
 package xyz.wagyourtail.jsmacros.client.mixin.access;
 
-import net.minecraft.client.gui.screens.inventory.HorseInventoryScreen;
+import net.minecraft.client.gui.screens.inventory.AbstractMountInventoryScreen;
 import net.minecraft.world.entity.Entity;
-import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import xyz.wagyourtail.jsmacros.client.access.IHorseScreen;
 
-@Mixin(HorseInventoryScreen.class)
-public class MixinHorseScreen implements IHorseScreen {
+@Mixin(AbstractMountInventoryScreen.class)
+public class MixinAbstractMountInventoryScreen {
     @Shadow
-    @Final
-    private AbstractHorse horse;
+    protected LivingEntity mount;
 
     @Override
     public Entity jsmacros_getEntity() {
-        return horse;
+        return mount;
     }
 
 }
---
+++ common/src/main/java/xyz/wagyourtail/jsmacros/util/TextUtil.java
@@ -0,0 +1,4 @@
+package xyz.wagyourtail.jsmacros.util;
+
+public class TextUtil {
+}
